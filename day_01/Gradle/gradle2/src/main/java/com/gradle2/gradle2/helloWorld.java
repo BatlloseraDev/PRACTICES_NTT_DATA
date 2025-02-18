d519ab02351d5a9fcb4188d743b45b474dbf5ae4
BatlloseraDev <batlloseradeveloper@gmail.com>
2025-02-18 12:13:16 +0100

    Initial commit with day01
    First commit with things of day one in Ntt data practices

---
+++ day_01/Gradle/gradle2/src/main/java/com/gradle2/gradle2/helloWorld.java
@@ -0,0 +1,8 @@
+package com.gradle2.gradle2;
+
+public class helloWorld {
+
+    public String hola(){
+        return "Hello world GRADLE";
+    }
+}
---
+++ day_01/Maven/maven/src/main/java/com/maven/maven/helloWorld.java
@@ -0,0 +1,9 @@
+package com.maven.maven;
+
+public class helloWorld
+{
+    public String hola(){
+        return "Hello world MAVEN";
+    }
+
+}
