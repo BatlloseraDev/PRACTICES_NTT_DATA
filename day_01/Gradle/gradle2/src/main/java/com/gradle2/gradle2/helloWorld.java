f4d29492e488d7d02c0ee98d08cb94eba70ed122
BatlloseraDev <batlloseradeveloper@gmail.com>
2025-02-18 13:25:42 +0100

    Error Solved
    Now it works with no problem with the corrections applied

--- day_01/Gradle/gradle2/src/main/java/com/gradle2/gradle2/helloWorld.java
+++ day_01/Gradle/gradle2/src/main/java/com/gradle2/gradle2/helloWorld.java
@@ -1,7 +1,13 @@
 package com.gradle2.gradle2;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class helloWorld {
 
+    @GetMapping
     public String hola(){
         return "Hello world GRADLE";
     }
--- day_01/Maven/maven/src/main/java/com/maven/maven/helloWorld.java
+++ day_01/Maven/maven/src/main/java/com/maven/maven/helloWorld.java
@@ -1,7 +1,13 @@
 package com.maven.maven;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class helloWorld
 {
+    @GetMapping("/")
     public String hola(){
         return "Hello world MAVEN";
     }
